fb425d0aa06f00d57479cc2b5ce49cd04034eb6c
wiemjlassi <wiemjlassi34@gmail.com>
2026-05-06 19:54:17 +0100

    fix bugs in article

--- backend/src/Repository/TagRepository.php
+++ backend/src/Repository/TagRepository.php
@@ -9,6 +9,10 @@ use Doctrine\Persistence\ManagerRegistry;
 /** @extends ServiceEntityRepository<Tag> */
 class TagRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tag::class);
+    }
     /** @return array<int, mixed> */
     public function findAllWithCount(): array
     {
